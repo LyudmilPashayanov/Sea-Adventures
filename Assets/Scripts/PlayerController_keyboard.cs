3447e03a7b0cc321f55943425a0441b4cc826e1a
Lyudmil Pashayanov <i385085@student.fontys.nl>
2020-09-06 22:48:30 +0200

    different controllers added / borders added / free 3d island added

---
+++ Assets/Scripts/PlayerController_keyboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController_keyboard : MonoBehaviour
+{
+    public float m_ForwardSpeed = 0.4f;
+    public float m_BackwardsSpeed = 0.2f;
+    public float m_RotationSpeed=1.5f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * m_ForwardSpeed, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * m_BackwardsSpeed, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * m_RotationSpeed, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * m_RotationSpeed, Space.Self);
+        }
+    }
+}
--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -8,56 +8,81 @@ public class cameraController : MonoBehaviour
     public float ScrollSpeed = 15;
     void Update()
     {
-        
-        if (Input.mousePosition.y >= Screen.height * 0.95)
+        if (Input.mousePosition.y >= Screen.height * 0.95f)
         {
-            if(Input.mousePosition.x >= Screen.width * 0.95) 
+            if (Input.mousePosition.x >= Screen.width * 0.95f)
             {
-                transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed/2, Space.World);;
-            }
-            else if (Input.mousePosition.x <= 100) 
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed/2, Space.World);
-            }
-            if (transform.localPosition.x <= -100)
-            {
-                return;
-            }
-           
-            transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
-        }
-        else if (Input.mousePosition.y <= 100) 
-        {
-            if (Input.mousePosition.x >= Screen.width * 0.95)
-            {
-                transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed/2, Space.World);
+                if (transform.localPosition.z <= -15)
+                {
+                }
+                else
+                {
+                    transform.Translate(0, 0, -1 * Time.deltaTime * ScrollSpeed / 2, Space.World);
+                }
             }
             else if (Input.mousePosition.x <= 100)
             {
-                transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed/2, Space.World);
+                if (transform.localPosition.z >= 85)
+                {
+                }
+                else
+                {
+                    transform.Translate(0, 0, 1 * Time.deltaTime * ScrollSpeed / 2, Space.World);
+                }
             }
             if (transform.localPosition.x >= 0)
             {
                 return;
             }
-            transform.Translate(Vector3.left * Time.deltaTime * ScrollSpeed, Space.World);
+           
+           
+            transform.Translate(1 * Time.deltaTime * ScrollSpeed, 0,0, Space.World);
         }
-        else if (Input.mousePosition.x >= Screen.width * 0.95)
+        else if (Input.mousePosition.y <= 100) 
+        {
+            if (Input.mousePosition.x >= Screen.width * 0.95f)
+            {
+                if (transform.localPosition.z <= -15)
+                {
+                }
+                else
+                {
+                    transform.Translate(0, 0, -1 * Time.deltaTime * ScrollSpeed / 2, Space.World);
+                }
+            }
+            else if (Input.mousePosition.x <= 100)
+            {
+                if (transform.localPosition.z >= 85)
+                {
+                }
+                else
+                {
+                    transform.Translate(0, 0, 1 * Time.deltaTime * ScrollSpeed / 2, Space.World);
+                }
+            }
+            if (transform.localPosition.x <= -100)
+            {
+                return;
+            }
+            transform.Translate(-1 * Time.deltaTime * ScrollSpeed, 0,0 , Space.World);
+        }
+
+
+        else if (Input.mousePosition.x >= Screen.width * 0.95f)
         {
             if (transform.localPosition.z <= -15)
             {
                 return;
             }
-            transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed, Space.World);
+            transform.Translate(0, 0, -1 * Time.deltaTime * ScrollSpeed, Space.World);
         }
         else if (Input.mousePosition.x <= 100)
         {
             if (transform.localPosition.z >= 85)
             {
                 return;
-            }
-          
-            transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
+            }         
+            transform.Translate(0, 0, 1 * Time.deltaTime * ScrollSpeed, Space.World);
         }
     }
 }
