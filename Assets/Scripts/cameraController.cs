32ba95e2c576dbb5b1726ee8d8cd239cc9ddad7a
i385085 <l.pashayanov@student.fontys.nl>
2020-09-06 14:39:26 +0200

    first stuff

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,63 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [Range(0.0f, 100.0f)]
+    public float ScrollSpeed = 15;
+    void Update()
+    {
+        
+        if (Input.mousePosition.y >= Screen.height * 0.95)
+        {
+            if(Input.mousePosition.x >= Screen.width * 0.95) 
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed/2, Space.World);;
+            }
+            else if (Input.mousePosition.x <= 100) 
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed/2, Space.World);
+            }
+            if (transform.localPosition.x <= -100)
+            {
+                return;
+            }
+           
+            transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed, Space.World);
+        }
+        else if (Input.mousePosition.y <= 100) 
+        {
+            if (Input.mousePosition.x >= Screen.width * 0.95)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed/2, Space.World);
+            }
+            else if (Input.mousePosition.x <= 100)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed/2, Space.World);
+            }
+            if (transform.localPosition.x >= 0)
+            {
+                return;
+            }
+            transform.Translate(Vector3.left * Time.deltaTime * ScrollSpeed, Space.World);
+        }
+        else if (Input.mousePosition.x >= Screen.width * 0.95)
+        {
+            if (transform.localPosition.z <= -15)
+            {
+                return;
+            }
+            transform.Translate(Vector3.back * Time.deltaTime * ScrollSpeed, Space.World);
+        }
+        else if (Input.mousePosition.x <= 100)
+        {
+            if (transform.localPosition.z >= 85)
+            {
+                return;
+            }
+          
+            transform.Translate(Vector3.forward * Time.deltaTime * ScrollSpeed, Space.World);
+        }
+    }
+}
---
+++ Assets/Shader/Particle.shader
@@ -0,0 +1,79 @@
+﻿Shader "Lyudmil/Particle Blended" 
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags 
+		{
+			"RenderType"="Transparent"
+			"Queue"="Transparent" 
+			"PreviewType"="Plane"
+		}
+		Blend SrcAlpha OneMinusSrcAlpha 
+		ZWrite Off
+
+		Pass
+		{
+			CGPROGRAM
+			#include "UnityCG.cginc"
+	
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_instancing
+
+			sampler2D _MainTex;
+			fixed4 _MainTex_ST;
+
+			struct appdata 
+			{
+				float4 vertex : POSITION;
+				fixed2 uv : TEXCOORD0;
+				// fixed4 color : COLOR;
+				fixed3 normal : NORMAL;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+			
+			struct v2f 
+			{
+				float4 pos : SV_POSITION;
+				fixed4 posWorld : TEXCOORD1;	
+				fixed2 uv : TEXCOORD0;
+				// fixed4 color : COLOR;
+				fixed3 normal : NORMAL;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_TRANSFER_INSTANCE_ID(v, o);
+				
+
+
+				v.vertex = mul(unity_WorldToObject, v.vertex);
+				
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.posWorld = mul(unity_ObjectToWorld, v.vertex);
+				o.normal = mul (UNITY_MATRIX_MV, v.normal);
+				// o.color = v.color;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_TARGET
+			{
+				fixed4 mainTex = tex2D(_MainTex, i.uv);
+
+				return mainTex;
+			}
+
+			ENDCG
+		}
+	}
+	Fallback "Curved Unlit Surface"
+}
